02a0df0309518f073a8e162f001f44d94596594b
zdz <2549626703@qq.com>
2023-05-21 00:32:37 +0800

    feat: add remote demo

--- main.js
+++ main.js
@@ -1,43 +1,72 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron")
 
-function createWindow () {
-  // Create the browser window.
+app.on('ready', () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      contextIsolation: false, // 高版本的electron默认为true，配合nodeIntegration
+      nodeIntegration: true, // 渲染进程运行使用commonjs
+      enableRemoteModule: true, // 使用remote模块
     }
   })
+  mainWindow.loadFile('./index.html')
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools() // 默认打开dev开发者工具
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+  // 开启 @electron/remote  start
+  require("@electron/remote/main").initialize();
+  require("@electron/remote/main").enable(mainWindow.webContents);  // 使用remote模块
+  // 开启 @electron/remote  end
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  ipcMain.on('message', (event, arg) => {
+    console.log('show message of ipcRenderer: ', arg)
+    console.log('event', event)
+    event.reply('reply', 'i get it!')
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
 
-// In this file you can include the rest of your app's specific main process
+// // Modules to control application life and create native browser window
+// const { app, BrowserWindow } = require('electron')
+// const path = require('path')
+
+// function createWindow () {
+//   // Create the browser window.
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   // and load the index.html of the app.
+//   mainWindow.loadFile('index.html')
+
+//   // Open the DevTools.
+//   // mainWindow.webContents.openDevTools()
+// }
+
+// // This method will be called when Electron has finished
+// // initialization and is ready to create browser windows.
+// // Some APIs can only be used after this event occurs.
+// app.whenReady().then(() => {
+//   createWindow()
+
+//   app.on('activate', function () {
+//     // On macOS it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// })
+
+// // Quit when all windows are closed, except on macOS. There, it's common
+// // for applications and their menu bar to stay active until the user quits
+// // explicitly with Cmd + Q.
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+
+// // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -5,3 +5,29 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+const { ipcRenderer } = require('electron')
+
+const remote = require('@electron/remote') // <==> 老版本 require('electron').remote
+const { BrowserWindow } = require('@electron/remote')
+
+console.log('eee', BrowserWindow, remote)
+
+window.addEventListener('DOMContentLoaded', () => {
+    const mesBnt = document.getElementById('mesBnt')
+    mesBnt.addEventListener('click', () => {
+      console.log(5454)
+      ipcRenderer.send('message', 'hahaha from renderer')
+
+      let win = new BrowserWindow({
+        with: 500,
+        height: 800
+      })
+      win.loadURL('https://baidu.com')
+    })
+
+    ipcRenderer.on('reply', (event, arg) => {
+      console.log('i get your reply:', arg)  
+      document.getElementById('mesBackBnt').innerHTML = arg 
+    })
+})
